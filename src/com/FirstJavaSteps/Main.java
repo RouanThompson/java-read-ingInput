da2706b7672e41c3713a4e1a97121fb6467db732
rouanthompson <rouan.thompson@gmail.com>
2021-04-10 21:05:30 -0400

    Read a byte from terminal

--- src/com/FirstJavaSteps/Main.java
+++ src/com/FirstJavaSteps/Main.java
@@ -1,8 +1,17 @@
 package com.FirstJavaSteps;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+        // Make a new Scanner object called "scanner"
+        // System.in reads data from the terminal
+
+        System.out.print("Age: ");
+        byte age = scanner.nextByte();
+        // reads and return the next byte
+        System.out.println("You are " + age);
     }
 }
